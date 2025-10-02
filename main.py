30f8f1d70b802ecb68846013ad020a70c40c57ea
Dmitry Karpenko <dkarp3ncko@yandex.ru>
2025-10-02 19:00:16 +0500

    add cors policy

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI, UploadFile
-from starlette.applications import Starlette
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["http://127.0.0.1:8000"],
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
 
 @app.post("/upload_file")
 async def upload_file(file: UploadFile):
