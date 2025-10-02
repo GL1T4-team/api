a7631544dcf5ef28182eb9faf7cb35faccfbd1b8
Dmitry Karpenko <dkarp3ncko@yandex.ru>
2025-10-02 18:32:33 +0500

    bugfixes

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI, UploadFile
+from starlette.applications import Starlette
+from starlette.responses import PlainTextResponse
+
+from content_size_limit_asgi import ContentSizeLimitMiddleware
 
 app = FastAPI()
+app.add_middleware(ContentSizeLimitMiddleware, max_content_size=20_000_000)
 
 @app.post("/upload_file")
 async def upload_file(file: UploadFile):
