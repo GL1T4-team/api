474e82ebb6fe827f05e7b56f5706b2b0aca93adf
Dmitry Karpenko <dkarp3ncko@yandex.ru>
2025-10-02 21:32:39 +0500

    update cors policy

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["*"],
+    allow_origins = ["http://127.0.0.1:8000"],
     allow_methods = ["*"],
     allow_headers = ["*"]
 )
